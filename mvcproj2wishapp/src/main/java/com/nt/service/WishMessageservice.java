7ac49c4353ba9ad7545728b5f6304dd022d82581
sardar <coolsaddam1@gmail.com>
2020-09-22 21:28:35 +0530

    secondapp

---
+++ mvcproj2wishapp/src/main/java/com/nt/service/WishMessageservice.java
@@ -0,0 +1,22 @@
+package com.nt.service;
+
+import java.util.Calendar;
+
+public class WishMessageservice implements WishService {
+
+	@Override
+	public String wishmessage() {
+		int  hour=0;
+		Calendar calender=Calendar.getInstance();
+		hour=calender.HOUR_OF_DAY;
+		if(hour<12) 
+			return "good morning";
+		
+		else if(hour<16)
+			return "good afternoon";
+		else if(hour<20)
+			
+		
+	}
+
+}
